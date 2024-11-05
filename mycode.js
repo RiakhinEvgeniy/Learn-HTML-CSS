9c53e101f98e39050a58ba7eb76985d672adf248
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-05 11:52:45 +0100

    Prototypes chain

--- mycode.js
+++ mycode.js
@@ -1,17 +1,23 @@
-// Создаем объект machine с методом start
-let machine = {
-    start: function() {
-        console.log("Start");
+// Создаем объект organism с методом live
+let organism = {
+    live: function() {
+        console.log("Living");
     }
 };
 
-// Создаем объект robot, который наследует от machine
-let robot = Object.create(machine);
-
-// Добавляем метод stop в прототип объекта machine
-machine.stop = function() {
-    console.log("Stop");
+// Создаем объект animal с методом move и устанавливаем его прототипом organism
+let animal = Object.create(organism);
+animal.move = function() {
+    console.log("Moving");
 }
 
-// Вызываем метод stop у объекта robot
-robot.stop();
+// Создаем объект bird с методом fly и устанавливаем его прототипом animal
+let bird = Object.create(animal);
+bird.fly = function() {
+    console.log("Flying");
+;}
+
+// Вызываем методы live, move и fly у объекта bird
+bird.live(); // Living
+bird.move(); // Moving
+bird.fly();  // Flying
