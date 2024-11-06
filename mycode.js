9ee8cfaa6e4758e622ed3516eb1062e0fd71703a
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-06 11:34:15 +0100

    Static methods

--- mycode.js
+++ mycode.js
@@ -1,27 +1,12 @@
-// Объявление класса Animal
-class Animal {
-    constructor(name) {
-        this.name = name;
+class MathHelper {
+    static subtract(a, b) {
+        return a - b;
     }
 
-    speak() {
-        return console.log(`${this.name} makes a noise`);
-    }
-}
-
-class Predator extends Animal {
-    constructor(name) {
-        super(name);
-    }
-
-    hunt() {
-        return console.log(`${this.name} is hunting`)
+    static divide(a, b) {
+        return a / b; 
     }
 }
 
-// Создание экземпляра класса Predator
-let predator = new Predator("Lion");
-
-// Вызов методов speak и hunt для экземпляра класса Predator
-predator.speak(); // Вывод: Lion makes a noise.
-predator.hunt();  // Вывод: Lion is hunting.
+console.log(MathHelper.subtract(100, 50));
+console.log(MathHelper.divide(21, 7));
