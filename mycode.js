70df966602e0e46cfea51a591934b062be1daa01
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-06 11:11:41 +0100

    Employee and Manager. Extending

--- mycode.js
+++ mycode.js
@@ -1,25 +1,21 @@
-class Library {
-
-    books = [];
-
-    constructor(name) {
+class Employee {
+    constructor(name, salary) {
         this.name = name;
-    }
-
-    addBook(nameOfBook) {
-        this.books.push(nameOfBook);
-    }
-
-    listBooks() {
-        return this.books;
+        this.salary = salary;
     }
 }
 
-// Пример использования и тестирования
-const myLibrary = new Library('My Library');
+class Manager extends Employee {
+    constructor(name, salary, department) {
+        super(name, salary);
+        this.department = department;
+    }
+}
 
-myLibrary.addBook('To Kill a Mockingbird');
-myLibrary.addBook('1984');
-myLibrary.addBook('The Great Gatsby');
+// Создание экземпляра класса Employee
+const employee = new Employee("John Doe", 50000);
+console.log(`Employee Name: ${employee.name}, Salary: ${employee.salary}`);
 
-console.log(myLibrary.listBooks()); // ['To Kill a Mockingbird', '1984', 'The Great Gatsby']
+// Создание экземпляра класса Manager
+const manager = new Manager("Jane Doe", 70000, "Sales");
+console.log(`Manager Name: ${manager.name}, Salary: ${manager.salary}, Department: ${manager.department}`);
