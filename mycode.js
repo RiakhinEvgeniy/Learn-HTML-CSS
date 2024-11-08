3ddb265bd203e1fedddd35ed055d890db30ca1c2
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-08 17:38:28 +0100

    Change styles

--- mycode.js
+++ mycode.js
@@ -1,6 +1,12 @@
-let elP = document.getElementById('myParagraph')
-elP.addEventListener('click', () =>{
-    console.log(elP.classList.contains('highlight'));
-    elP.classList.toggle('highlight')
-    console.log(elP.classList.contains('highlight'));
-});
+let elP = document.getElementById('myParagraph');
+let styles = window.getComputedStyle(elP);
+console.log(styles.color)
+console.log(styles.backgroundColor)
+console.log(styles.fontSize)
+elP.style.color = 'blue'
+elP.style.backgroundColor = 'yellow'
+elP.style.fontSize = '20px'
+let styles1 = window.getComputedStyle(elP);
+console.log(styles1.color)
+console.log(styles1.backgroundColor)
+console.log(styles1.fontSize)
