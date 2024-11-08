3a5de959a766f1398bc01c6bb024b5f2ac5736b9
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-08 16:28:09 +0100

    Added class to element

--- mycode.js
+++ mycode.js
@@ -1,5 +1,6 @@
-let elementDiv = document.createElement("div")
-elementDiv.textContent = 'Это новый элемент';
-let el_Body = document.querySelector("body")
-let elH1 = document.querySelector("h1")
-el_Body.insertBefore(elementDiv, elH1)
+let elP = document.getElementById('myParagraph')
+elP.addEventListener('click', () =>{
+    console.log(elP.classList.contains('highlight'));
+    elP.classList.toggle('highlight')
+    console.log(elP.classList.contains('highlight'));
+});
