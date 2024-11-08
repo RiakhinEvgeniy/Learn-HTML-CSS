d1243b526f6d4e87d83416c872347bda18ef0bf0
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-08 13:47:53 +0100

    querySelectorAll

--- mycode.js
+++ mycode.js
@@ -1,4 +1,2 @@
-let elementUl = document.getElementById("myList");
-console.log(elementUl.innerHTML);
-elementUl.innerHTML += "<li>New Item</li>";
-console.log(elementUl.innerHTML);
+let allParagraph = document.querySelectorAll(".container p");
+console.log(allParagraph.length);
