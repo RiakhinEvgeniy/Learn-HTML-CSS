e4ca0218255945eed6a4c058920565373a2104a4
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-11 11:10:03 +0100

    Moving mouse in element div

--- mycode.js
+++ mycode.js
@@ -1,12 +1,4 @@
-let btn_1 = document.getElementById('button1');
-let btn_2 = document.getElementById('button2');
-
-function clickButton1() {
-    console.log('Кнопка была нажата');
-}
-
-btn_1.addEventListener('click', clickButton1);
-btn_2.addEventListener('click', function() {
-    btn_1.removeEventListener('click', clickButton1)
-    console.log('Listener button 1 removed');
+let elDiv = document.getElementById('mouseArea');
+let mouseMove = elDiv.addEventListener('mousemove', (move) => {
+    console.log(`${move.clientX} * ${move.clientY}`)
 });
