d864d54934c647bebeaf079f7e412c4b9a3935cb
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-11 09:54:07 +0100

    ADD: addEventListener

--- mycode.js
+++ mycode.js
@@ -1,12 +1,8 @@
-let elP = document.getElementById('myParagraph');
-let styles = window.getComputedStyle(elP);
-console.log(styles.color)
-console.log(styles.backgroundColor)
-console.log(styles.fontSize)
-elP.style.color = 'blue'
-elP.style.backgroundColor = 'yellow'
-elP.style.fontSize = '20px'
-let styles1 = window.getComputedStyle(elP);
-console.log(styles1.color)
-console.log(styles1.backgroundColor)
-console.log(styles1.fontSize)
+let btn = document.getElementById('myButton');
+btn.addEventListener('click', function() {
+    console.log('Первый обработчик')
+});
+
+btn.addEventListener('click', function() {
+    console.log('Второй обработчик')
+});
