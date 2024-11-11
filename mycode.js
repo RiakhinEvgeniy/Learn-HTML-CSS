ec0cae5536191675c4e8bea21696a5367036a837
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-11 10:08:18 +0100

    ADD: removeEventListener

--- mycode.js
+++ mycode.js
@@ -1,8 +1,12 @@
-let btn = document.getElementById('myButton');
-btn.addEventListener('click', function() {
-    console.log('Первый обработчик')
-});
+let btn_1 = document.getElementById('button1');
+let btn_2 = document.getElementById('button2');
 
-btn.addEventListener('click', function() {
-    console.log('Второй обработчик')
+function clickButton1() {
+    console.log('Кнопка была нажата');
+}
+
+btn_1.addEventListener('click', clickButton1);
+btn_2.addEventListener('click', function() {
+    btn_1.removeEventListener('click', clickButton1)
+    console.log('Listener button 1 removed');
 });
