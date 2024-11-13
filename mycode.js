24fae77dffa03863040a7d5b21e543727c8c4575
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-13 20:51:38 +0100

    Canvas. Rectangle. Circle

--- mycode.js
+++ mycode.js
@@ -1,9 +1,12 @@
 const canvas = document.getElementById('myCanvas');
-      const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d');
+        
+        // Рисование синего прямоугольника
+        ctx.fillStyle = 'blue'
+        ctx.fillRect(10, 10, 150, 100);
 
-      ctx.beginPath();
-      ctx.moveTo(50, 150);
-      ctx.lineTo(250, 150);
-      ctx.lineTo(150, 300);
-      ctx.closePath();
-      ctx.stroke();
+        // Рисование зеленого круга
+        ctx.beginPath();
+        ctx.arc(300, 75, 50, 0, Math.PI * 2);
+        ctx.fillStyle = 'red'
+        ctx.fill();
