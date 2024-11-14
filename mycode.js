bf62ea4e8c80f2500ccdfee97ea53166cf876e28
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-14 14:09:38 +0100

    Promise then, catch, finally

--- mycode.js
+++ mycode.js
@@ -1,28 +1,23 @@
-function loadImage() {
-      return new Promise((resolve) => {
-            setTimeout(() => {
-                  resolve('Loaded image.');
-            }, 1000)
+const myPromise = new Promise((resolve, reject) => {
+      // Симулируем асинхронную операцию
+      setTimeout(() => {
+          const success = false; // Попробуйте поменять на false для теста reject
+          if (success) {
+              resolve("Операция завершена успешно!");
+          } else {
+              reject("Произошла ошибка!");
+          }
+      }, 1000);
+  });
 
-      })
-}
-
-//loadData
-function loadData() {
-      return new Promise((resolve) => {
-            setTimeout(() => {
-                  resolve('Loaded data.');
-            }, 2000)
-
-      })
-}
-
-//All resources loaded
-Promise.all([loadImage(), loadData()])
-.then(res => {
-      console.log('All resources loaded');
+  myPromise
+  .then((res) => {
       console.log(res);
-})
-.catch(error => {
+  })
+  .catch((error) => {
       console.log(error);
-});
+  })
+  .finally(() => {
+      console.log('Finally!');
+  })
+  
