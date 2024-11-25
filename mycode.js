cf4fda6b530f044ec94b541e454b62564df59fa9
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-25 12:12:43 +0100

    ERROR GLOBAL: with Promice reject

--- mycode.js
+++ mycode.js
@@ -1,26 +1,21 @@
-// Объявление асинхронной функции fetchDataAsync
-async function fetchDataAsync() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-    let err = Math.random() > 0.5 ? true : false;
-    if(!err) {
-      reject(err);
-    } else {
-      resolve({id: 1, name: 'Evgeniy'});
-    }
-  }, 1000);
-  })
+// Установка глобального обработчика ошибок для window.onerror
+window.onerror = function(message, source, lineno, colno, error) {
+  console.error(`Ошибка: ${message} в ${source}:${lineno}:${colno}`, error);
+};
+
+// Установка глобального обработчика ошибок для window.onunhandledrejection
+window.onunhandledrejection = function (event) {
+  console.error(event.reason);
 }
 
-// Объявление асинхронной функции loadDataAsync
-async function loadDataAsync() {
-  try {
-    let data = await fetchDataAsync();
-    console.log(data);
-  } catch(error) {
-    console.log(error);
-  }
-}
+// Асинхронная функция, которая генерирует ошибку через 1 секунду
+async function generateErr() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  throw new Error('Error from generateERR');
+};
 
-// Вызов функции loadDataAsync для тестирования
-loadDataAsync();
+// Создание отклоненного промиса
+
+Promise.reject(new Error('Errors from Promice reject'));
+// Вызов асинхронной функции для генерации ошибки
+let e = generateErr();
