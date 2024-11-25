e83be9e489a818db8e7016f6b8b7b84bab09136e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-25 11:29:54 +0100

    ERROR: with asynchronoys methods

--- mycode.js
+++ mycode.js
@@ -1,13 +1,26 @@
-let axios = require('axios');
+// Объявление асинхронной функции fetchDataAsync
+async function fetchDataAsync() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    let err = Math.random() > 0.5 ? true : false;
+    if(!err) {
+      reject(err);
+    } else {
+      resolve({id: 1, name: 'Evgeniy'});
+    }
+  }, 1000);
+  })
+}
 
-let updatePost = async (data) => {
+// Объявление асинхронной функции loadDataAsync
+async function loadDataAsync() {
   try {
-    let url = 'https://jsonplaceholder.typicode.com/posts/1'
-    let response = await axios.put(url, data);
-    console.log(response.data);
+    let data = await fetchDataAsync();
+    console.log(data);
   } catch(error) {
-    console.error(error);
+    console.log(error);
   }
 }
 
-updatePost({title: 'Updated Post', body: 'This post has been updated', userId: 1});
+// Вызов функции loadDataAsync для тестирования
+loadDataAsync();
