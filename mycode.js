b53cb619900a5e57eb2cbd6fe572972d294bccfa
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-27 11:10:54 +0100

    Method apply

--- mycode.js
+++ mycode.js
@@ -1,21 +1,6 @@
-// Конструктор Animal
-function Animal(name, species) {
-    this.name = name;
-    this.species = species;
-}
-
-// Конструктор Dog, который наследует свойства от Animal с использованием call
-function Dog(name, species) {
-    Animal.call(this, name, species);
-}
-
-// Добавление метода bark в конструктор Dog
-Dog.prototype.bark = function() {
-    console.log(`Woof! I'm ${this.name}, the ${this.species}`);
-}
-
-// Создание экземпляра Dog
-const myDog = new Dog('Buddy', 'dog');
-
-// Проверка работы метода bark
-myDog.bark(); // Вывод: "Woof! I'm Buddy, the dog"
+function maxOfArray(numbers) {
+    return Math.max.apply(null, numbers);
+    }
+    
+    const testArray = [3, 5, 7, 2, 8];
+    console.log(maxOfArray(testArray)); // Вывод: 8
