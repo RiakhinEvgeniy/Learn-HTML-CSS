7bf01def93454f5a0bda874b7cb89ba0ca899b07
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-12-03 15:44:38 +0100

    Map

--- mycode.js
+++ mycode.js
@@ -1,21 +1,10 @@
-let email = new Set();
+let userRole = new Map();
+userRole.set('Evgeniy', 'ADMIN');
+userRole.set('Nataliy', 'editor');
+userRole.set('Vlad', 'viewer');
+userRole.set('Anna', 'user');
+userRole.set('David', 'manager');
 
-email.add('ryahin@gmail.com');
-email.add('ryahinr.e.u@gmail.com');
-email.add('apellesodeskiy@gmail.com');
-email.add('ryahin@gmail.com');
-email.add('ryahina@gmail.com');
-email.add('ryahina2@gmail.com');
-
-// Проверка наличия одного из email
-
-console.log(email.has('ryahinr.e.u@gmail.com'))
-// Удаление одного из email
-email.delete('ryahina@gmail.com')
-// Вывод всех уникальных email адресов с использованием цикла for...of
-for (const userEmail of email) {
-    console.log(userEmail);
+for (const [name, role] of userRole) {
+    console.log(`${name}: ${role}`);
 }
-// Очистка Set, удаление всех email адресов
-email.clear();
-console.log(email.size);
