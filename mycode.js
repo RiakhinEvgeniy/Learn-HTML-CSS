0840420b04abd8c697e832d316d321f0453a5234
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-12-20 21:29:40 +0100

    Function with a loop over an object

--- mycode.js
+++ mycode.js
@@ -1,12 +1,33 @@
+
 async function testAxios(url) {
+    let obj;
     await axios.get(url)
-    .then((respones) => {
-        console.log(respones.data);
-    })
-    .catch(err => {
-        console.log(err.name);
-    })
+        .then((respones) => {
+            obj = respones.data
+        })
+        .catch(err => {
+            console.log(err.name);
+        })
+
+    loopOnObject(obj);
 }
 
 const url = 'https://jsonplaceholder.typicode.com/users/1'
 testAxios(url);
+
+function loopOnObject(objectWithData) {
+    for (let key in objectWithData) {
+        if (objectWithData.hasOwnProperty(key)) {
+
+            const value = objectWithData[key];
+
+            if (typeof value === 'object' && value !== null) {
+                loopOnObject(value)
+            }
+            else {
+                // console.log(`${key}: ${value}`);
+                console.log(key, value);
+            }
+        }
+    }
+}
